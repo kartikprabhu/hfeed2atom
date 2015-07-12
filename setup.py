c787c0eeafd8781550823ba09c2f171b131dd9ab
Kartik <me@kartikprabhu.com>
2015-07-12 04:31:34 -0500

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+# use requirements.txt for dependencies
+with open('requirements.txt') as f:
+    required = map(lambda s: s.strip(), f.readlines())
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='hfeed2atom',
+    version='0.1.0',
+    description='h-feed to Atom converter',
+    long_description=readme,
+    install_requires=required,
+    author='Kartik Prabhu',
+    author_email='me@kartikprabhu.com',
+    url='https://github.com/kartikprabhu/hfeed2atom',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
