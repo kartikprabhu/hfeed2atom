326af8999570e5d4f972409270c34dad3060b26a
Kartik <me@kartikprabhu.com>
2015-07-17 00:04:12 -0500

    request the doc if None found.

--- hfeed2atom/feed_parser.py
+++ hfeed2atom/feed_parser.py
@@ -1,13 +1,27 @@
 import mf2py
 from bs4 import BeautifulSoup
 
+import requests
+
+
 def feed_parser(doc=None, url=None):
 	"""
 	parser to get hfeed
 	"""
 
-	if not isinstance(doc, BeautifulSoup):
-		doc = BeautifulSoup(doc)
+	if doc:
+		if not isinstance(doc, BeautifulSoup):
+			doc = BeautifulSoup(doc)
+
+	if url:
+		if doc is None:
+			data = requests.get(url)
+
+			# check for charater encodings and use 'correct' data
+			if 'charset' in data.headers.get('content-type', ''):
+				doc = BeautifulSoup(data.text)
+			else:
+				doc = BeautifulSoup(data.content)
 
 	# parse for microformats
 	parsed = mf2py.Parser(doc, url).to_dict()
